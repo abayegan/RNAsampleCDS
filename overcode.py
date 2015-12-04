a5c2a0d6b6e54232f84dd551c6f6a41254fe388f
amir <bayegan@bc.edu>
2015-12-04 02:46:06 -0500

    works for +1 frameshift- supports gc-content, weighted sampling and nucleotide constraints given in a file

--- overcode.py
+++ overcode.py
@@ -24,6 +24,9 @@ from rnaSeqBlosumCompatibleWithOverlappingReadingFrames import readBlosumMatrix
 
 NUCL   = ['A','C','G','U']
 STOP   = ['UAA', 'UGA', 'UAG']
+iupacDict = {'A':['A'],'C':['C'],'G':['G'],'U':['U'],'R':['A','G'],'Y':['C','U'],'S':['G','C'],
+			'W':['A','U'],'K':['G','U'],'M':['A','C'],'B':['C','G','U'],'D':['A','G','U'],
+			'H':['A','C','U'],'V':['A','C','G'],'N':['A','C','G','U']}
 DEBUG  = False
 PRINT  = True #set False if use main as function to return list
 
@@ -162,7 +165,7 @@ def createListOf4mersForAllPairsOfResidues(filename=None,threshold=1):
 					if aa1==aa1bis and aa2==aa2bis:
 						Tuples.append(tuple)
 				else: #use Blosum similarity; recall if aa==aabis then Blosum similar
-					if Blosum[aa1][aa1bis]>=threshold and  Blosum[aa2][aa2bis]>=threshold:            Tuples.append(tuple)
+					if Blosum[aa1][aa1bis]>=threshold and  Blosum[aa2][aa2bis]>=threshold: Tuples.append(tuple)
 			D[aa1+aa2] = Tuples
 	return D
 
@@ -172,49 +175,30 @@ def consistent(fourMer1,fourMer2):
 	else:
 		return False
 
-
-def computeZF0(n,constraint):
-	#Compute number of RNAs of len 3*n+1 that code in frames 0,1 and
-	#respect constraint
-	#
-	L = createListOfOverlappingCodons()
+def computeZB(peptide1,peptide2,D,constraint=None):
+	p = peptide1; q = peptide2
+	assert (len(p) <= len(q)) 
+	n  = min(len(p),len(q)) 
 	#------------Initialize ZF---------------------
-	ZF = {}
+	ZB = {}
 	for k in range(0,n+1):
 		for ch in NUCL:
-			ZF[(k,ch)] = 0.0
-	if constraint==None or constraint[0]=='N':
-		for ch in NUCL: ZF[(0,ch)] = 1.0
-	else: #first position is constrained     
-		ch = constraint[0]
-		ZF[(0,ch)] = 1.0
+			ZB[(k,ch)] = 0.0
+	for ch in NUCL: ZB[(n,ch)] = 1.0
 	#------------Fill matrix ZF-------------------
-	if constraint==None:
-		for k in range(1,n+1): #k in [1,...,n] 
-			for s in L:
-				ZF[(k,s[-1])] += ZF[(k-1,s[0])]
-	else: #must satisfy sequence constraint
-		for k in range(1,n+1): #k in [1,...,n] 
-			if len(constraint)<=3*(k-1):
-				constraint4tuple = ''
-			else:
-				constraint4tuple = constraint[3*(k-1):3*k+1]
-			L0                = []
-			for tuple in L:
-				ok = 1
-				for i in range(len(constraint4tuple)):
-					ch = constraint4tuple[i]
-					if ch!='N' and tuple[i]!=ch:
-						ok = 0
-						#break
-				if ok: 
-					L0.append(tuple)
-			if DEBUG:
-				print "computeZF ",k,L0
-			for s in L0:
-				ZF[(k,s[-1])] += ZF[(k-1,s[0])]
-	return ZF
-
+	for k in range(n-1,-1,-1): #k in [1,...,n] 
+		dimer = p[k]+q[k]
+		L     = D[dimer]
+		#~ print "tuples at level %s:"%k ,L
+		for s in L:
+			ZB[(k,s[0])] += ZB[(k+1,s[-1])]
+	ZFval  = 0.0
+	for ch in NUCL: ZFval += ZB[(0,ch)]			
+	print '%f'%ZFval
+	#~ for key in sorted(ZB):
+		#~ print key,ZB[key]
+	return ZB
+	
 def computeZF(peptide1,peptide2,D,constraint=None): 
 	#forwards "partition function"
 	# Here, peptide1 = p_{0}...p_{n-1} and peptide2 = q_{0}...q_{n-1}
@@ -246,8 +230,8 @@ def computeZF(peptide1,peptide2,D,constraint=None):
 	if constraint==None or constraint[0]=='N':
 		for ch in NUCL: ZF[(0,ch)] = 1.0
 	else: #first position is constrained     
-		ch = constraint[0]
-		ZF[(0,ch)] = 1.0
+		for ch in constraint[0]:
+			ZF[(0,ch)]=1.0
 	#------------Fill matrix ZF-------------------
 	if constraint==None:
 		for k in range(1,n+1): #k in [1,...,n] 
@@ -259,7 +243,17 @@ def computeZF(peptide1,peptide2,D,constraint=None):
 		for k in range(1,n+1): #k in [1,...,n] 
 			dimer            = p[k-1]+q[k-1]
 			L0               = D[dimer]
-			end(tuple)
+			constraint4tuple = constraint[3*(k-1):3*k+1]
+			L                = []
+			for tuple in L0:
+				ok = 1
+				for i in range(len(constraint4tuple)):
+					ch = constraint4tuple[i]
+					if tuple[i] not in iupacDict[ch]:
+						ok = 0
+						#break
+				if ok: 
+					L.append(tuple)
 			if DEBUG:
 				print "computeZF ",k,L
 			for s in L:
@@ -293,33 +287,37 @@ def computeZF_GC(peptide1,peptide2,D,constraint=None):
 	n  = min(len(p),len(q)) 
 	#------------Initialize ZF---------------------
 	ZF = {}
-	#ZF1={}
 	for k in range(0,n+1):
 		for ch in NUCL:
-			#ZF1[(k,ch)]=0.0
 			for x in range(0,3*k+6): #Warning: 3k+2 doesn't work.
 				ZF[(k,x,ch)] = 0.0
-	ZF[(0,1,'C')]=1.0
-	ZF[(0,1,'G')]=1.0
 	if constraint==None or constraint[0]=='N':
-			dimer = p[0]+q[0]
-			L     = D[dimer]
-			for s in L:
-				ZF[(1,GCcont(s),s[-1])] += 1.0
-				#ZF1[(1,s[-1])] +=1.0
-			if(DEBUG==1):
-				print "tuples at level 1:", L
-				for a in NUCL:
-					for x in range(0,5):
-						print 'ZF[1,%d,%s]:%d' % (x,a,ZF[(1,x,a)])
-	else: #first position is constrained
 		dimer = p[0]+q[0]
 		L     = D[dimer]
 		for s in L:
+			ZF[(1,GCcont(s),s[-1])] += 1.0
+		if(DEBUG==1):
+			print "tuples at level 1:", L
+			for a in NUCL:
+				for x in range(0,5):
+					print 'ZF[1,%d,%s]:%d' % (x,a,ZF[(1,x,a)])
+	else: #first tuple is constrained
+		dimer = p[0]+q[0]
+		L0     = D[dimer]
+		constraint4tuple = constraint[:4]
+		L=[]
+		for tuple in L0:
+			print tuple
+			ok = 1
+			for i in range(len(constraint4tuple)):
+				ch = constraint4tuple[i]
+				if tuple[i] not in iupacDict[ch]:
+					ok = 0
+					#break
+			if ok: 
+				L.append(tuple)
+		for s in L:
 				ZF[(1,GCcont(s),s[-1])] += 1.0    
-		ch = constraint[0]
-		if(ch=='C' or ch=='G'):
-		ZF[(0,1,ch)] = 1.0
 	#------------Fill matrix ZF-------------------
 	if constraint==None:
 		for k in range(2,n+1):
@@ -345,24 +343,19 @@ def computeZF_GC(peptide1,peptide2,D,constraint=None):
 		for k in range(1,n+1): #k in [1,...,n] 
 			dimer            = p[k-1]+q[k-1]
 			L0               = D[dimer]
-			if len(constraint)<=3*(k-1):
-				constraint4tuple = ''
-			else:
-				constraint4tuple = constraint[3*(k-1):3*k+1]
+			constraint4tuple = constraint[3*(k-1):3*k+1]
 			L                = []
 			for tuple in L0:
 				ok = 1
 				for i in range(len(constraint4tuple)):
 					ch = constraint4tuple[i]
-					if ch!='N' and tuple[i]!=ch:
+					if tuple[i] not in iupacDict[ch]:
 						ok = 0
 						#break
 				if ok: 
 					L.append(tuple)
 			for s in L:
-				#ZF1[(k,s[-1])]+=ZF1[(k-1,s[0])]
 				for x in range(0,3*k+2):
-					#print s,x,GCcont(s[1:4])
 					if(x-GCcont(s[1:])>=0):
 						ZF[(k,x,s[-1])] += ZF[(k-1,x-GCcont(s[1:]),s[0])]
 			if(DEBUG==1):
@@ -387,7 +380,7 @@ def getNucSampleProb(ZF,k,nuclist):
 		probdict[nuc]=(ZF[k,nuc]/sum)
 	return probdict
 
-def sample(peptide1,peptide2,seqLen,numSamples,D,constraint,weighted):
+def sample(peptide1,peptide2,seqLen,numSamples,replacement,D,constraint,weighted):
 	#D[4-tuple] is list of "similar" 4-tuplesaa1+aa2]
 	#Here, the key 4-tuple codes aa1 in frame 0 and aa2 in frame 1
 	#4-tuples in list L=D[4-tuple] code aa1' in frame 0 and aa2' in frame 1
@@ -426,57 +419,73 @@ def sample(peptide1,peptide2,seqLen,numSamples,D,constraint,weighted):
 			if constraint==None: 
 				Tuples = Tuples0 #Warning: this sets Tuples to point to Tuples0
 			else:
-				if len(constraint)<=3*(k-1):
-					constraint4tuple = ''
-				else:
-					constraint4tuple = constraint[3*(k-1):3*k+1]
+				constraint4tuple = constraint[3*(k-1):3*k+1]
 				for tuple in Tuples0:
 					ok = 1
 					for i in range(len(constraint4tuple)):
 						ch = constraint4tuple[i]
-						if ch!='N' and tuple[i]!=ch:
+						if tuple[i] not in iupacDict[ch]:
 							ok = 0
 							break
-					if ok: 
+					if ok:
 						Tuples.append(tuple)
 			#-----------------------------------------------
 			#choose random tuple with first char ch1 for which ZF[(k-1,ch1)]>0
 			#-----------------------------------------------
 			print 'tuples before filteration:'
 			print Tuples
-			if weighted==1:
-				selTuples=[];nuclist=[]
-				for tup in Tuples: #get the set of nucleotides in t[0]
-						if nuclist==NUCL: break
-						if tup[0] not in nuclist: nuclist.append(tup[0])
-				print 'nuclist', nuclist
-				sampleDict = getNucSampleProb(ZF,k-1,nuclist)
-				print sampleDict
-				ch1 = rouletteWheel(sampleDict)
-				while ZF[(k-1,ch1)]==0: ch1 = rouletteWheel(sampleDict)
-				print 'sampled ch1',ch1
-				for tup in Tuples:
-					#print tup
-					if(tup[0]==ch1 and k!=1): 
-						selTuples.append(tup)
-					if(k==1): #Warning: since ZF[0,nuc]=1 (nuc in {A,C,G,U}) the first nucleotide does not need to be ch1 and can be anything 
-						selTuples = Tuples
-				print 'tuples after filteration:'
-				print selTuples
-				tuple = random.choice(selTuples)
-				print "selected tuple:",tuple
-			else:
-				#print Tuples
-				tuple = random.choice(Tuples)
-				while ZF[(k-1,tuple[0])]==0: tuple = random.choice(Tuples)
-				
-				#print (k-1),tuple,tuple[0],ZF[(k-1,tuple[0])],"+++"
-			#ZF[(k,tuple[3])] -= 1.0
-			print "-1:",k,tuple[3]
-			rna = tuple[1]+tuple[2]+tuple[3]+rna
-			print rna
-			ch4  = tuple[0] #set ch for next round
-			k   = k-1
+			if (replacement==1):
+				if weighted==1:
+					selTuples=[];nuclist=[]
+					for tup in Tuples: #get the set of nucleotides in t[0]
+							if nuclist==NUCL: break
+							if tup[0] not in nuclist: nuclist.append(tup[0])
+					print 'nuclist', nuclist
+					sampleDict = getNucSampleProb(ZF,k-1,nuclist)
+					print sampleDict
+					ch1 = rouletteWheel(sampleDict)
+					while ZF[(k-1,ch1)]==0: ch1 = rouletteWheel(sampleDict)
+					print 'sampled ch1',ch1
+					for tup in Tuples:
+						#print tup
+						if(tup[0]==ch1 and k!=1): 
+							selTuples.append(tup)
+						if(k==1): #Warning: since ZF[0,nuc]=1 (nuc in {A,C,G,U}) the first nucleotide does not need to be ch1 and can be anything 
+							selTuples = Tuples
+					print 'tuples after filteration:'
+					print selTuples
+					tuple = random.choice(selTuples)
+					print "selected tuple:",tuple
+				else: #uniform sampling
+					#print Tuples
+					tuple = random.choice(Tuples)
+					while ZF[(k-1,tuple[0])]==0: tuple = random.choice(Tuples)
+					
+					#print (k-1),tuple,tuple[0],ZF[(k-1,tuple[0])],"+++"
+				#ZF[(k,tuple[3])] -= 1.0
+				print "-1:",k,tuple[3]
+				rna = tuple[1]+tuple[2]+tuple[3]+rna
+				print rna
+				ch4  = tuple[0] #set ch for next round
+				k   = k-1
+			else: #sampling w/o replacement
+				ZB = computeZB(peptide1,peptide2,D,constraint)
+				sorted(ZB)
+				print "ZB"
+				for key in ZB:
+					print key,ZB[key]
+				random.shuffle(Tuples)
+				print 'tuples before filteration:'
+				print Tuples
+				for tuple in Tuples:	
+					if(ZF[(k-1,tuple[0])]>=0 and ZB[(k,tuple[-1])]>0):
+						break
+				print 'selected tuple'
+				print tuple
+				ZB[k,tuple[-1]]-=1
+				rna = tuple[1]+tuple[2]+tuple[3]+rna
+				ch4  = tuple[0] #set ch for next round
+				k   = k-1		
 		rna = ch4+rna #append the first character, which remains to be appended
 		for k in sorted(ZF):
 			print k,ZF[k]
@@ -522,15 +531,12 @@ def sample_GC(peptide1,peptide2,seqLen,numSamples,GC,D,constraint,weighted):
 			if constraint==None: 
 				Tuples = Tuples0 #Warning: this sets Tuples to point to Tuples0
 			else:
-				if len(constraint)<=3*(k-1):
-					constraint4tuple = ''
-				else:
-					constraint4tuple = constraint[3*(k-1):3*k+1]
+				constraint4tuple = constraint[3*(k-1):3*k+1]
 				for tuple in Tuples0:
 					ok = 1
 					for i in range(len(constraint4tuple)):
 						ch = constraint4tuple[i]
-						if ch!='N' and tuple[i]!=ch:
+						if tuple[i] not in iupacDict[ch]:
 							ok = 0
 							break
 					if ok: 
@@ -580,10 +586,11 @@ def main0(n,constraint=None):
 	print "%f" % ZFval
 	return ZFval
 
-def main(peptide1,peptide2,numSamples,weighted,gc,filename=None,threshold=1,constraint=None):
+def main(peptide1,peptide2,numSamples,weighted,gc,replacement,filename=None,threshold=1,constraint=None):
 	D      = createListOf4mersForAllPairsOfResidues(filename,threshold)
 	seqLen = min(len(peptide1),len(peptide2))
 	if (gc==-1):
+		ZB     = computeZB(peptide1,peptide2,D,constraint)
 		ZF     = computeZF(peptide1,peptide2,D,constraint)
 		if DEBUG:
 			for ch in NUCL:
@@ -592,7 +599,7 @@ def main(peptide1,peptide2,numSamples,weighted,gc,filename=None,threshold=1,cons
 					sys.stdout.write( "\tZF[(%s,%s)] = %s\n  " % (k,ch,ZF[(k,ch)]) )
 		ZFval  = 0.0
 		for ch in NUCL: ZFval += ZF[(seqLen,ch)]
-		RNA    = sample(peptide1,peptide2,seqLen,numSamples,D,constraint,weighted)
+		RNA    = sample(peptide1,peptide2,seqLen,numSamples,replacement,D,constraint,weighted)
 	else:
 		ZF     = computeZF_GC(peptide1,peptide2,D,constraint)			
 		ZFval  = 0.0
@@ -606,9 +613,26 @@ def main(peptide1,peptide2,numSamples,weighted,gc,filename=None,threshold=1,cons
 			print GCcont(rna)
 	return ZFval,RNA
 
+def parseConstraintFile(fname,n):
+	if not (os.path.isfile(fname)):
+		print "\nconstraint file does not exist!\n"
+		sys.exit(1)
+	else:
+		f = open(fname,'r')
+		const = f.readline().strip()
+		print 'constraints:',const
+		if len(const)!= 3*n+1: 
+			print "\nerror: constraint should have length 3n+1 where n is the length of the given peptides\n"
+			sys.exit(1)
+		for i in const:
+			if i not in iupacDict.keys():
+				print "\nconstraint file contains character %s which is not a nucleotide iupac code\n"
+				sys.exit(1)
+	return const
+	
 def printusage(exe):
 	text = """
-Usage: %s peptide1 peptide2 -n numSamples -c constraint -m BlosumMatrix -t threshold -gc GC_content -w
+Usage: %s peptide1 peptide2 -n numSamples -c constraint -m BlosumMatrix -t threshold -gc GC_content -r 0|1 -w
 
 1) peptides given in IUPAC single letter codes and must have same length
 2) numSamples is desired number of samples to generate from PSSM defined
@@ -618,7 +642,8 @@ Usage: %s peptide1 peptide2 -n numSamples -c constraint -m BlosumMatrix -t thres
 4) threshold in Blosum62 for codons that translate to 
 		residues with >= threshold similarity (default +1)
 5) GC-content of the sampled sequences
-6) Representative sampling """ % exe
+6) sampling with or w/o replacement
+7) Representative sampling """ % exe
 	print text
 	text = """
 This code can be used to do following:
@@ -655,16 +680,17 @@ if __name__ == '__main__':
 	threshold  = 1
 	weighted = -1
 	gc=-1
+	replacement=1
 	constraint = None
 	args     = sys.argv[3:]
 	#sequence constraint indicated by -c flag
 	for i in range(len(args)):
 		if args[i]=='-c':
-			constraint = args[i+1]
+			constraintfile = args[i+1]
 			del[args[i+1]]; del[args[i]]
 			break
 		else:
-			constraint = None
+			constraintfile = None
 	#number of samples, with -n flag
 	for i in range(len(args)):
 		if args[i]=='-n':
@@ -698,6 +724,15 @@ if __name__ == '__main__':
 				printusage(sys.argv[0])
 			del[args[i+1]]; del[args[i]]
 			break
+	#sampling with or w/o replacement with -r 1 / -r 0 flage respectively.
+	for i in range(len(args)):
+		if args[i]=='-r':
+			replacement = int(args[i+1])
+			if(replacement!=0 and replacement!=1):
+				print "\nerror in -r input!"
+				printusage(sys.argv[0])
+			del[args[i+1]]; del[args[i]]
+			break
 	#weighted sampling, with -w flag
 	for i in range(len(args)):
 		if args[i]=='-w':
@@ -705,5 +740,8 @@ if __name__ == '__main__':
 			break
 	n   = len(peptide1)
 #  main0(n,constraint)
-	main(peptide1,peptide2,numSamples,weighted,gc,filename,threshold,constraint)
-
+	if(constraintfile):
+		constraint = parseConstraintFile(constraintfile,n)
+	else:
+		constraint = None
+	main(peptide1,peptide2,numSamples,weighted,gc,replacement,filename,threshold,constraint)
